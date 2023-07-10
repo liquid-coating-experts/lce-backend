6f1bbf7b9c7b2bfccb606f4b95f886377f38528b
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2023-07-10 12:57:19 +0100

    update config for digital o

--- medusa-config.js
+++ medusa-config.js
@@ -28,8 +28,17 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
+// const DATABASE_URL =
+//   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_DATABASE = process.env.DB_DATABASE;
+
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` +
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -99,6 +108,8 @@ const projectConfig = {
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL,
+
+  database_extra: { ssl: { rejectUnauthorized: false } },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
